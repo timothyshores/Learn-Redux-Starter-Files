9865315cfbcb2260c5a094f4b42f82a0de0226bd
Timothy Shores <timothymshores@gmail.com>
2019-03-04 21:22:47 -0600

    Setup redux store

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Main = React.createClass({
-    render() {
-        return (
-            <div>
-                <h1>
-                    <Link to="/">Reduxgram</Link>
-                </h1>
-            </div>
-        )
-    }
+   render() {
+      return (
+         <div>
+            <h1>
+               <Link to="/">Reduxgram</Link>
+            </h1>
+            {React.cloneElement(this.props.children, this.props)}
+         </div>
+      )
+   }
 });
 
 export default Main;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
 import css from './styles/style.styl';
-import Main from './components/Main';
 
-render(<Main />, document.getElementById('root'));
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:photoId" component={Single} />
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
